2f144a25b2013806450f8a9d00a41fac28462fcf
Pavel547 <pawukok11@gmail.com>
2025-03-25 20:52:37 +0100

    feaat: I add a registration for my site

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.UserLoginView.as_view(), name='login'),
+    path("registration/", views.registration, name="registration"),
+    path("login/", views.UserLoginView.as_view(), name="login"),
     path("logout/", views.UserLogoutView.as_view(), name="logout"),
     path("reset-password/", views.ResetPasswordView.as_view(), name="reset-password")
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordChangeView
+from .forms import RegistrationForm
 
 class UserLoginView(LoginView):
     template_name = "registration/login.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["loginform"] = context.pop("form")
+        return context
+    
 
 class UserLogoutView(LogoutView):
     template_name = "registration/logout.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["logoutform"] = context.pop("form")
+        return context
 
 class ResetPasswordView(PasswordResetView):
     template_name = "registration/reset_password.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["resetpassword_form"] = context.pop("form")
+        return context
+    
+def registration(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("users:login")  
+    else:
+        form = RegistrationForm()
+    return render(request, "registration/registration.html", {"registrationform": form})
